ff553e66427f028d21e694b43a9f74b0efb576f2
komxun <komxun@gmail.com>
2023-07-10 15:48:18 +0100

    add saveVid to weather_custom

--- Weather_map_custom.m
+++ Weather_map_custom.m
@@ -1,20 +1,23 @@
 clc, clear, close all
 
+saveVid = 0;   % 1: save  2: no
+
 % number of point
 Nx = 200;
 Ny = 200;
-tmax = 100;
+tmax = 1;
 
 weatherMat = zeros(Nx, Ny, tmax);
-numSeed = 8187;
+% numSeed = 8187;
+numSeed = 123;
 
 figure(88)
 axis equal tight
 colormap turbo
 
 for t = 1:tmax
-    f = 0.015;   % Increase the randomness control parameter
-    
+%     f = 0.015;   % Increase the randomness control parameter
+    f = 0.05;
     rand('seed', numSeed)
     
     nx1 = 1 + floor(0.999*f*Nx);
@@ -37,17 +40,25 @@ for t = 1:tmax
     imagesc(F)
     
     hold on 
-    [C2,h2] = contourf(1:200, 1:200, F, [1, 1], 'FaceAlpha',0,...
-        'LineColor', 'w', 'LineWidth', 2);
+%     [C2,h2] = contourf(1:200, 1:200, F, [1, 1], 'FaceAlpha',0,...
+%         'LineColor', 'w', 'LineWidth', 2);
     
-    clabel(C2,h2,'FontSize',15,'Color','w')
+%     clabel(C2,h2,'FontSize',15,'Color','w')
     axis equal tight
     
-    title(num2str(t,'time = %4.1f s'))
+    
+    title("Seed: " + num2str(numSeed) + ", f = " + num2str(f))
+%     subtitle(num2str(t,'time = %4.1f s'))
     set(gca, 'YDir', 'normal')
     colorbar
     pause(0.01)
     hold off
+
+    % Video saving
+    if saveVid
+        frm(t) = getframe(gcf) ;
+        drawnow
+    end
 
     weatherMat(:,:,t) = F;
 end
@@ -55,6 +66,24 @@ end
 set(gca, 'YDir', 'normal')
 fileName = "WeatherMat_" + num2str(numSeed) +".mat";
 % save(fileName, 'weatherMat')
+
+if saveVid
+    video_name = "WeatherMat_" + num2str(numSeed) + ".avi";
+    % create the video writer with 1 fps
+    writerObj = VideoWriter(video_name);
+    writerObj.FrameRate = 30;
+    % set the seconds per image
+    % open the video writer
+    open(writerObj);
+    % write the frames to the video
+    for i=1:length(frm)
+        % convert the image to a frame
+        frame = frm(i) ;    
+        writeVideo(writerObj, frame);
+    end
+    % close the writer object
+    close(writerObj);
+end
 
 
 
